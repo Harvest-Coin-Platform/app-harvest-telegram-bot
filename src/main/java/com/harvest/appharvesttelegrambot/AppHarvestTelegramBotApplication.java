4ec7ceb6fd5a8608bbf3c5f77b80e5cd2da6a540
someliukh <stepan.omeliukh.work@gmail.com>
2024-06-20 01:34:52 +0300

    Init commit

---
+++ src/main/java/com/harvest/appharvesttelegrambot/AppHarvestTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.harvest.appharvesttelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppHarvestTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppHarvestTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harvest/appharvesttelegrambot/AppHarvestTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harvest.appharvesttelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppHarvestTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
